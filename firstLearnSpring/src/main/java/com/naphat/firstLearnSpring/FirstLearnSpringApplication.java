28bd6f83d38cb4205e928c580f7c6544789e3924
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-16 18:54:54 +0700

    import File

---
+++ firstLearnSpring/src/main/java/com/naphat/firstLearnSpring/FirstLearnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.naphat.firstLearnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLearnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLearnSpringApplication.class, args);
+	}
+
+}
---
+++ firstLearnSpring/src/test/java/com/naphat/firstLearnSpring/FirstLearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naphat.firstLearnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLearnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
