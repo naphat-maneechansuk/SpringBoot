c886ec01a6e94605609e385c413ab8fdf5b86178
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-17 15:06:55 +0700

    Learn 17_7_2568

--- firstLearnSpring/src/main/java/com/naphat/firstLearnSpring/FirstLearnSpringApplication.java
+++ firstLearnSpring/src/main/java/com/naphat/firstLearnSpring/FirstLearnSpringApplication.java
@@ -1,7 +1,9 @@
 package com.naphat.firstLearnSpring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirstLearnSpringApplication {
@@ -9,5 +11,10 @@ public class FirstLearnSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstLearnSpringApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner->{
+			System.out.println("Hello i na luck");
+		};
+	}
 }
